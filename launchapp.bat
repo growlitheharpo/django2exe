d3e5fdd40c151fca0164ee8e170229497a357f6a
James Keats <growlitheharpo@gmail.com>
2019-08-07 19:47:35 -0400

    Fix directory changes in batch files

--- launchapp.bat
+++ launchapp.bat
@@ -1,4 +1,8 @@
 @echo off
+pushd %~dp0
+
 call py-dist\scripts\env.bat
 cd py-dist\
+
 python run.py
+popd
--- setup.bat
+++ setup.bat
@@ -1,3 +1,7 @@
+@echo off
+pushd %~dp0
+
 call py-dist\scripts\env.bat
 cd requirements\
 pip install -r setup_requirements.txt
+popd
