d9e8d2e1c062438ef9e3a581136b41b449a767cb
James Keats <growlitheharpo@gmail.com>
2019-08-07 20:29:25 -0400

    Totally gut env.bat (it is no longer needed)

--- py-dist/scripts/env.bat
+++ py-dist/scripts/env.bat
@@ -1,10 +1,1 @@
 @echo off
-
-set HOME=%WINPYDIR%\..\settings
-set WINPYARCH="WIN32"
-if  "%WINPYDIR:~-5%"=="amd64" set WINPYARCH="WIN-AMD64"
-
-set PATH=%WINPYDIR%\Lib\site-packages\PyQt5;%WINPYDIR%\Lib\site-packages\PyQt4;%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%WINPYDIR%\..\tools;%WINPYDIR%\..\tools\mingw32\bin;%WINPYDIR%\..\tools\R\bin\i386;%WINPYDIR%\..\tools\Julia\bin;%PATH%;
-
-rem force default Qt kit for Spyder
-set QT_API=pyqt
